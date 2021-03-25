310bd2b910c5a85e08d55027a0045937ff9d78ca
KavyaChoudhary2008 <72203225+KavyaChoudhary2008@users.noreply.github.com>
2021-03-25 12:48:28 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,90 @@
+var helicopterIMG, helicopterSprite, packageSprite,packageIMG;
+var packageBody,ground
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+function preload()
+{
+	helicopterIMG=loadImage("helicopter.png")
+	packageIMG=loadImage("package.png")
+}
+
+function setup() {
+	createCanvas(800, 700);
+	rectMode(CENTER);
+	
+
+	packageSprite=createSprite(width/2, 80, 10,10);
+	packageSprite.addImage(packageIMG)
+	packageSprite.scale=0.2
+
+	helicopterSprite=createSprite(width/2, 200, 10,10);
+	helicopterSprite.addImage(helicopterIMG)
+	helicopterSprite.scale=0.6
+
+	groundSprite=createSprite(width/2, height-35, width,10);
+	groundSprite.shapeColor=color(255)
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:0.4, isStatic:true});
+	World.add(world, packageBody);
+	
+
+	//Create a Ground
+	ground = Bodies.rectangle(width/2, 650, width, 10 , {isStatic:true} );
+ 	World.add(world, ground);
+x=400;
+y=650;
+
+	 baseBody=Bodies.rectangle(x,y,200,20)
+leftBody=Bodies.rectangle(x-100,y-50,20,100)
+rightBody=Bodies.rectangle(x+100,y-50,20,100)
+
+World.add(world,baseBody)
+World.add(world,leftBody)
+World.add(world,rightBody)
+
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+  packageSprite.x= packageBody.position.x 
+  packageSprite.y= packageBody.position.y 
+  rect(x,y,200,20)
+  rect(x-100,y-50,20,100)
+  rect(x+100,y-50,20,100)
+  drawSprites();
+ 
+}
+
+function keyPressed() {
+ if (keyCode === DOWN_ARROW) {
+    // Look at the hints in the document and understand how to make the package body fall only on press of the Down arrow key.
+Matter.Body.setStatic(packageBody,false);
+    
+  }
+
+  else if(keyCode===LEFT_ARROW){
+helicopterSprite.x=helicopterSprite.x-20;
+translation={x:-20,y:0}
+Matter.Body.translate(packageBody,translation)
+  }
+
+  else if(keyCode===RIGHT_ARROW){
+	helicopterSprite.x=helicopterSprite.x+20;
+	translation={x:20,y:0}
+Matter.Body.translate(packageBody,translation)
+}
+}
+
+
+
